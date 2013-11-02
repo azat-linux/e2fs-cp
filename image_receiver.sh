f2e2ab91381e964fdb396598fb3f7c1fcf0319ae
Azat Khuzhin <a3at.mail@gmail.com>
2013-11-02 23:28:45 +0400

    image_receiver: add more todo's

--- image_receiver.sh
+++ image_receiver.sh
@@ -5,5 +5,7 @@ port=255
 
 for fs in $*; do
     nc -nlp $port | tee -a >(md5sum >&2) | dd of=$fs oflag=direct iflag=fullblock bs=16k
+    # TODO: do we need to calc md5sum from $fs
+    # TODO: e2fsck of $fs
 done
 
