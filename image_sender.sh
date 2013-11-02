fe07520a38af7908ab14ece7e52f4baaa8ee4429
Azat Khuzhin <a3at.mail@gmail.com>
2013-11-02 23:25:33 +0400

    image_sender: drop incrementing of port, since copying is sequental
    Since the bandwidth nto so huge.

--- image_sender.sh
+++ image_sender.sh
@@ -7,7 +7,7 @@
 # NOTE: md5sum is 300-400 MB/s so it won't slow down copying
 #
 
-start_port=255
+port=255
 bs=16k
 
 function get_image_size_in_bs()
@@ -18,9 +18,7 @@ function get_image_size_in_bs()
 
 set -e
 
-port=$start_port
 for fs in $*; do
     dd if=$fs bs=$bs iflag=direct count=$(get_image_size_in_bs $fs) | tee >(md5sum >&2) | nc -q1 dst $port
-    port=$(( port + 1 ))
 done
 
