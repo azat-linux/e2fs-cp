8fa18edcafe4bdd8fe22df8d2e3214fede7effc5
Azat Khuzhin <a3at.mail@gmail.com>
2013-11-02 23:32:00 +0400

    image_receiver: add set-e

--- image_receiver.sh
+++ image_receiver.sh
@@ -3,6 +3,8 @@
 # TODO: add common consts
 port=255
 
+set -e
+
 for fs in $*; do
     nc -nlp $port | tee -a >(md5sum >&2) | dd of=$fs oflag=direct iflag=fullblock bs=16k
     # TODO: do we need to calc md5sum from $fs
