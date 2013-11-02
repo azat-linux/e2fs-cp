23970c9de1f7843630e9c36e1de3645f1a298aa2
Azat Khuzhin <a3at.mail@gmail.com>
2013-11-02 23:40:13 +0400

    resize_copy: bypass filesystems arguments

--- resize_copy.sh
+++ resize_copy.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+#
+# Aggregate functional of all helpers
+# from this utils
+#
+
 function copy_scripts()
 {
     ssh $1 mkdir -p ~/resize_copy
@@ -11,11 +16,13 @@ set -x
 
 dst=$1
 src=$2
+shift
+shift
 
 copy_scripts $src
 copy_scripts $dst
 
-ssh $src ~/resize_copy/resize2fs.sh
-ssh $dst ~/resize_copy/image_receiver.sh &
-ssh $src ~/resize_copy/image_sender.sh
+ssh $src ~/resize_copy/resize2fs.sh $*
+ssh $dst ~/resize_copy/image_receiver.sh $* &
+ssh $src ~/resize_copy/image_sender.sh $dst $*
 
