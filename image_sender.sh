ce1aef9524162d5154721b486cd802f5c9d771d4
Azat Khuzhin <a3at.mail@gmail.com>
2013-11-02 23:24:11 +0400

    image_sender: add get_image_size_in_bs and use it
    To copy only real size of fs, not the whole partition
    (because we didn't do fdisk)

--- image_sender.sh
+++ image_sender.sh
@@ -8,12 +8,19 @@
 #
 
 start_port=255
+bs=16k
+
+function get_image_size_in_bs()
+{
+    local bs_size=$(df $1 -B$bs | awk '{print $2}')
+    echo $bs_size
+}
 
 set -e
 
 port=$start_port
 for fs in $*; do
-    dd if=$fs bs=16K iflag=direct | tee >(md5sum >&2) | nc -q1 dst $port
+    dd if=$fs bs=$bs iflag=direct count=$(get_image_size_in_bs $fs) | tee >(md5sum >&2) | nc -q1 dst $port
     port=$(( port + 1 ))
 done
 
