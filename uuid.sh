8714db604fdf8f78d3cffa9e20187ee4625526f1
Azat Khuzhin <a3at.mail@gmail.com>
2013-11-03 13:48:26 +0400

    uuid: fix incrementing of get_uuid_i variable

--- uuid.sh
+++ uuid.sh
@@ -3,17 +3,17 @@
 set -e
 set -x
 
-get_uuid_i=1
 function get_uuid()
 {
     # TODO: or accept mount points instead of block devices
     fstab_line=$(grep ext4 /etc/fstab | egrep -v ' (/usr|/|/home|/var|) ' | tail -n+$get_uuid_i | head -n1)
-    let ++get_uuid_i
 
     echo "$fstab_line" | cut -d' ' -f1 | cut -d= -f2
 }
 
+get_uuid_i=1
 for fs in $*; do
     tune2fs $fs -U $(get_uuid $fs)
+    let ++get_uuid_i
 done
 
