6578e07501bf4bad6e05acf3141108bf62ea9c37
Azat Khuzhin <a3at.mail@gmail.com>
2013-11-03 11:31:13 +0400

    uuid: initial implementation

---
+++ uuid.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+function last_mount_point()
+{
+    dumpe2fs -h $1 2>/dev/null | grep 'Last mounted on' | awk '{print $NF}'
+}
+
+function get_uuid()
+{
+    grep $(last_mount_point $1) /etc/fstab | cut -d' ' -f1 | cut -d= -f2
+}
+
+for fs in $*; do
+    tune2fs $fs -U $(get_uuid $fs)
+done
+
