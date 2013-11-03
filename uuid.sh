970a08e1a28fe67f78287ace4db848dcfeef525f
Azat Khuzhin <a3at.mail@gmail.com>
2013-11-03 13:40:19 +0400

    uuid: don't use 'last mount point', since it will be copied from src fs

--- uuid.sh
+++ uuid.sh
@@ -3,14 +3,14 @@
 set -e
 set -x
 
-function last_mount_point()
-{
-    dumpe2fs -h $1 2>/dev/null | grep 'Last mounted on' | awk '{print $NF}'
-}
-
+get_uuid_i=1
 function get_uuid()
 {
-    grep $(last_mount_point $1) /etc/fstab | cut -d' ' -f1 | cut -d= -f2
+    # TODO: or accept mount points instead of block devices
+    fstab_line=$(grep ext4 /etc/fstab | egrep -v ' (/usr|/|/home|/var|) ' | tail -n+$get_uuid_i | head -n1)
+    let ++get_uuid_i
+
+    echo "$fstab_line" | cut -d' ' -f1 | cut -d= -f2
 }
 
 for fs in $*; do
