95385f6dfecdbd5608e784b1063c9fbcd7b8c77b
Azat Khuzhin <a3at.mail@gmail.com>
2013-11-03 16:55:05 +0400

    resize2fs: use resize2fs minimal calculation algorithm and check it

--- resize2fs.sh
+++ resize2fs.sh
@@ -23,6 +23,17 @@ mkdir -p $logs
 
 # resize
 (for fs in $*; do
-    echo "$fs $(get_min_block_size_kb $fs)K >& $logs/$(basename $fs).resize2fs.log"
+    min_size_4k=$(resize2fs -P $fs 2>/dev/null | awk '{print $NF}')
+    min_size_kb=$(( min_size_4k * 4 ))
+    diff=$(( $min_size_kb - $(get_min_block_size_kb $fs) ))
+
+    if [ $diff -lt 0 ]; then
+        echo "$fs: resize2fs: malformed min size" >&2
+        continue
+    else
+        echo "$fs: resize2fs: optimal size greater up to $(( $diff / 1024 / 1024 ))G" >&2
+    fi
+
+    echo "$fs ${min_size_kb}K >& $logs/$(basename $fs).resize2fs.log"
 done) | xargs -I{} -P10 bash -c "time resize2fs {}"
 
