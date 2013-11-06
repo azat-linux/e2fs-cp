bfaf24938f9a96a30f7dfe20805ef1023b89427e
Azat Khuzhin <a3at.mail@gmail.com>
2013-11-06 15:48:56 +0400

    test.sh: initial implementation

---
+++ test.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+#
+#
+#
+
+function image_info()
+{
+    df -B4K test.img
+    df -i test.img
+    dumpe2fs -h test.img
+}
+
+# create
+fallocate -l $((1024 * 1024 * 512)) test.img
+mke2fs -F -t ext4 test.img
+e2fsck test.img
+
+# mount
+mkdir -p mnt
+mount -o loop -t ext4 test.img mnt
+
+# fill it, with holes
+for i in {1..30}; do
+    dd if=/dev/urandom bs=1M count=1 >| mnt/${i}.test
+done
+for i in {1..30}; do
+    if [ $((i % 2)) -eq 0 ]; then
+        rm mnt/${i}.test
+    fi
+done
+for i in {1..30}; do
+    dd if=/dev/urandom bs=1M count=1 >| mnt/${i}.test
+done
+
+sync
+image_info test.img
+umount mnt
+
+min_size_4k=$(resize2fs -P test.img 2>/dev/null | awk '{print $NF}')
+resize2fs -M test.img
+e2fsck test.img
+
+# copy and check
+dd if=test.img of=test_dup.img bs=4k count=$min_size_4k oflag=direct
+sync
+image_info test_dup.img
+e2fsck test_dup.img
+
