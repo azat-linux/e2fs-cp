b04e2c532d40cba0f2597e5eb2c4acb4fa573c03
Azat Khuzhin <a3at.mail@gmail.com>
2013-11-08 20:10:32 +0400

    image_receiver: run fsck on dst image

--- image_receiver.sh
+++ image_receiver.sh
@@ -6,11 +6,15 @@ start_port=255
 set -e
 set -x
 
+function receive()
+{
+    nc -nlp $port | tee -a >(md5sum >&2) | dd of=$fs oflag=direct iflag=fullblock bs=16k
+}
+
 port=$start_port
 for fs in $*; do
-    (nc -nlp $port | tee -a >(md5sum >&2) | dd of=$fs oflag=direct iflag=fullblock bs=16k) &
+    receive $fs &
     # TODO: do we need to calc md5sum from $fs
-    # TODO: e2fsck of $fs
     let ++port
 done
 
