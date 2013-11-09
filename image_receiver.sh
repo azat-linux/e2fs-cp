6d2937c111378a01fd9560098cc4fb4619f65abc
Azat Khuzhin <a3at.mail@gmail.com>
2013-11-09 12:49:35 +0400

    image_receiver: check for filesystem is not mounted.

--- image_receiver.sh
+++ image_receiver.sh
@@ -10,6 +10,11 @@ function receive()
 {
     fs=$1
 
+    if $( df $fs | tail -n+2 | grep -q $fs ); then
+        echo "$fs is mounted" >&2
+        return
+    fi
+
     nc -nlp $port | tee -a >(md5sum >&2) | dd of=$fs oflag=direct iflag=fullblock bs=16k
     e2fsck $fs
 }
