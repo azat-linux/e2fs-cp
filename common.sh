554d1a3b0963d8fba49b72a43eea3b08c17ae3e7
Azat Khuzhin <a3at.mail@gmail.com>
2013-11-11 16:00:18 +0400

    common: print blocks info without traling whitespaces

--- common.sh
+++ common.sh
@@ -11,7 +11,7 @@ function get_min_fs_size_in_kb()
                        if ($1 == "Block size") bs=$NF; \
                        if ($1 == "Block count") count=$NF; \
                        if ($1 == "Free blocks") free=$NF; \
-                   } END {print bs count-free}')
+                   } END {printf "%.f %.f", bs+0, count-free}')
     local fs_bs_k=$(echo $dumpfs | awk '{print $1/1024}')
     local fs_used=$(echo $dumpfs | awk '{print $2}')
 
@@ -24,7 +24,7 @@ function get_fs_size_in_kb()
                    awk -F: '{\
                        if ($1 == "Block size") bs=$NF; \
                        if ($1 == "Block count") count=$NF; \
-                   } END {print bs count}')
+                   } END {printf "%.f %.f", bs, count}')
     local fs_bs_k=$(echo $dumpfs | awk '{print $1/1024}')
     local fs_count=$(echo $dumpfs | awk '{print $2}')
 
