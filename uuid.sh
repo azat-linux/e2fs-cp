cfbb50be63c64a8f768d1c09c13b19cb8fca1708
Azat Khuzhin <a3at.mail@gmail.com>
2013-11-11 15:17:47 +0400

    uuid: get_uuid(): add an argument, instead of global variable

--- uuid.sh
+++ uuid.sh
@@ -6,14 +6,14 @@ set -x
 function get_uuid()
 {
     # TODO: or accept mount points instead of block devices
-    fstab_line=$(grep ext4 /etc/fstab | egrep -v ' (/usr|/|/home|/var|) ' | tail -n+$get_uuid_i | head -n1)
+    fstab_line=$(grep ext4 /etc/fstab | egrep -v ' (/usr|/|/home|/var|) ' | tail -n+$1 | head -n1)
 
     echo "$fstab_line" | cut -d' ' -f1 | cut -d= -f2
 }
 
 get_uuid_i=1
 for fs in $*; do
-    tune2fs $fs -U $(get_uuid $fs)
+    tune2fs $fs -U $(get_uuid $get_uuid_i)
     let ++get_uuid_i
 done
 
