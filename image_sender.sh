09ad736403ff78dc39eecc529f7a65baf4cc94c2
Azat Khuzhin <a3at.mail@gmail.com>
2013-11-12 16:24:44 +0400

    image_sender: suppport one -> many servers.
    One of our use cases need it always.

--- image_sender.sh
+++ image_sender.sh
@@ -3,6 +3,10 @@
 #
 # NOTE: md5sum is 300-400 MB/s so it won't slow down copying
 #
+# Examples:
+#   One->One : ./image_sender dst.com /dev/sd?1
+#   One->Many: ./image_sender "dst1.com dst2.com" /dev/sd?1
+#
 
 . ${0%/*}/common.sh
 # TODO: handle mg/gb and other stuff
@@ -10,7 +14,7 @@ bs_k=${bs/k/}
 
 reserve=1000
 
-dst=$1
+dsts=$1
 shift
 
 set -e
@@ -21,6 +25,12 @@ for fs in $*; do
     fs_size=$(get_fs_size_in_kb $fs)
     fs_size_in_bs=$(( fs_size / bs_k ))
 
-    dd if=$fs bs=$bs iflag=direct count=$(( fs_size_in_bs + reserve)) | tee >(md5sum >&2) | nc -q1 $dst $port &
+    sender_cmd="dd if=$fs bs=$bs iflag=direct count=$(( fs_size_in_bs + reserve)) | tee >(md5sum >&2)"
+    for dst in $(echo $dsts); do
+        sender_cmd+=" | tee >(nc -q1 $dst $port)"
+    done
+    sender_cmd+=" > /dev/null"
+    bash -c "$sender_cmd" &
+
     let ++port
 done
