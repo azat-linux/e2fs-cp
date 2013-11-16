ccb31128a432150e9e4310487cd63df4441d7f56
Azat Khuzhin <a3at.mail@gmail.com>
2013-11-16 16:03:08 +0400

    get_uuid: fix grepping out of mounted disks

--- common.sh
+++ common.sh
@@ -45,7 +45,7 @@ function get_uuid()
 {
     # TODO: or accept mount points instead of block devices
     fstab_line=$(grep ext4 /etc/fstab | \
-                 egrep -v $(df | tail -n+2 | awk '{printf "%s|", $NF}' | sed 's/|$//') | \
+                 egrep -v "[\t ]$(df | tail -n+2 | awk '{printf "%s|", $NF}' | sed 's/|$//')[\t ]" | \
                  tail -n+$1 | \
                  head -n1)
 
