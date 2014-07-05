309aa5f07337177fab7c612ba61dc474348a4ff7
Azat Khuzhin <a3at.mail@gmail.com>
2014-07-05 12:07:15 +0400

    sync_uid_gid: resolve uid/gid conflicts

--- sync_uid_gid.sh
+++ sync_uid_gid.sh
@@ -6,12 +6,67 @@ orig_gids=$2
 uids_to_sync=$3
 gids_to_sync=$4
 
-for u in $uids_to_sync; do
-    o=$(grep "^${u}:" $orig_uids | cut -d: -f3)
-    usermod -u $o $u
-done
+u_start=1100
+g_start=1100
 
-for g in $gids_to_sync; do
-    o=$(grep "^${g}:" $orig_gids | cut -d: -f3)
-    groupmod -g $o $g
-done
+function match_id()
+{
+    [[ "$1" =~ [0-9]+ ]] && echo ${BASH_REMATCH[0]}
+}
+function get_login()
+{
+    getent $1 $2 | cut -d: -f1
+}
+function change_uid()
+{
+    while IFS=$'\n' read line; do
+        local uid=$(match_id "$line")
+        [ -z "$uid" ] && echo "$line" && continue
+        local login=$(get_login passwd $uid)
+
+        usermod -u $u_start $login
+
+        echo "Unplanned uid was changed, you porbably wan't to run:"
+        echo "find ~${login} -uid $uid -exec chown ${u_start} {} \\;"
+
+        let u_start++
+    done
+}
+function change_gid()
+{
+    while IFS=$'\n' read line; do
+        local gid=$(match_id "$line")
+        [ -z "$gid" ] && echo "$line" && continue
+        local group=$(get_login group $gid)
+
+        groupmod -g $g_start $group
+
+        echo "Unplanned gid was changed, you porbably wan't to run:"
+        echo "find / -gid $gid -exec chown :${g_start} {} \\;"
+
+        let g_start++
+    done
+}
+
+function uids()
+{
+    for u in $uids_to_sync; do
+        local o=$(grep "^${u}:" $orig_uids | cut -d: -f3)
+        while ! $( usermod -u $o $u 2> >( change_uid >&2 ) ); do
+            echo "Trying one more time"
+        done
+    done
+}
+
+function gids()
+{
+    for g in $gids_to_sync; do
+        local o=$(grep "^${g}:" $orig_gids | cut -d: -f3)
+        while ! $(groupmod -g $o $g 2> >( change_gid >&2 ) ); do
+            echo "Trying one more time"
+        done
+    done
+}
+
+uids
+gids
